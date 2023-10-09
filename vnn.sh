38d6d283e8eb090b74c3e51529ee95542bceef96
Danh Trần <116901931+vnn1489@users.noreply.github.com>
2023-10-09 07:27:49 +0700

    Update vnn.sh

--- vnn.sh
+++ vnn.sh
@@ -161,7 +161,7 @@ else
 
 
 # FOR CLIENT
-flatpak-i com.brave.Browser com.spotify.Client org.flameshot.Flameshot org.libreoffice.LibreOffice com.google.Chrome org.videolan.VLC org.ferdium.Ferdium
+flatpak-i com.brave.Browser com.spotify.Client org.flameshot.Flameshot org.libreoffice.LibreOffice com.google.Chrome org.videolan.VLC org.ferdium.Ferdium io.dbeaver.DBeaverCommunity
 snap-i code --classic
 apt-i tmux vim ibus-unikey git snapd curl net-tools openssh-server xz-utils at sshpass python3-pip ncdu solaar gnome-tweaks (use to when close screen, computer still run)
 app image: VirtualBox
