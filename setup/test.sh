97b1bc79a076c809a5407e5c35824c2b0acba608
vnn1489 <vnn1489@outlook.com>
2024-02-15 16:11:30 +0700

    Thu 20240215 09:11 (24h) UTC

--- setup/test.sh
+++ setup/test.sh
@@ -1,19 +1,1 @@
 #!/bin/bash
-
-delete_apps_default () {
-    apps'
-        libreoffice
-        aisleriot
-        gnome-mahjongg
-        gnome-mines
-        shotwell-common
-        shotwell
-        gnome-sudoku
-        gnome-todo-common
-        gnome-todo
-        libgnome-todo
-        transmission-common
-        cheese-common
-        cheese
-    '
-}
