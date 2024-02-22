6f79839afa520806a28a917439131cad42c3bc1a
vnn1489 <vnn1489@outlook.com>
2024-02-23 06:53:25 +0700

    Thu 20240222 23:53 (24h) UTC

--- setup/test.sh
+++ setup/test.sh
@@ -12,129 +12,36 @@ FILE_ERROR=error.log
 DIRECTORY_LOG=/var/opt/log
 FILE_RELEASE_INFO=/etc/os-release
 
-############# DEFINE FUNCTION
-config_git() {
-    GIT_CONFIG=~/.GITCONFIG
-
-    config() {
-
-CONFIG --GLOBAL USER.NAME "VNN1489"
-    GIT CONFIG --GLOBAL USER.EMAIL "VNN1489@OUTLOOK.COM"
-    echo "---> config git complete, check with command: cat $GIT_CONFIG"
-    }
-
-    if [ -f $GIT_CONFIG ]; then
-        if cat $GIT_CONFIG| grep -q 'vnn1489@outlook.com'; then
-            echo "---> existed info user"
-        else
-            config
-        fi
-    else
-        config
-    fi
-}
-
-check_error() {
-    if [ $? -ne 0 ]; then
-ERROR: run command to check: cat $DIRECTORY_LOG/$FILE_ERROR"
-ERROR: run command to check: cat $DIRECTORY_LOG/$FILE_ERROR"
-    fi
-}
-
-check_ssh_key() {
-    SSH_KEY=~/.ssh/id_rsa
-    
-    if [ -f $SSH_KEY ]; then
-        echo "---> add content of 'id_rsa.pub' to github, etc."
-    else
-        ssh-keygen -t rsa -N "" -f "$SSH_KEY" -q 1>> $DIRECTORY_LOG/$FILE_OK 2>> $FILE_ERROR
-    fi
-}
-
-update_app_apt() {
-    options='
-        upgrade
-        dist-upgrade
-        full-upgrade
-        autoremove
-    '
-
-    # this step, need command: echo "$password" | -S
-    for option in $options; do
-        echo "$password" | -S apt $option -y 1>> $DIRECTORY_LOG/$FILE_OK 2>> $DIRECTORY_LOG/$FILE_ERROR
-        check_error
-    done
-
-    apt list --upgradable | awk '{ print $1 }' | grep '/' | cut -d'/' -f1 |
-
+#
     while read -r PACKAGE_NAME; do
-        apt upgrade -y $PACKAGE_NAME 1>> $DIRECTORY_LOG/$FILE_OK 2>> $FILE_ERROR
-        check_error
-    done
+        apt upgrade -y $PACKAGE_NAME 1>> 
 }
 
-install_app_apt () {
-    echo "-------> installing apps with apt...."
+{
+...."
     apt update -y 1>> $DIRECTORY_LOG/$FILE_OK 2>> $DIRECTORY_LOG/$FILE_ERROR
     check_error
     update_app_apt
 
-    # APT: python3, net-tools, openssh-server, xz-utils, at, sshpass, python3-pip, bat
+    # APT: python3, net-tools, openssh-server, 
 
-    apps='
-        snap
-        flatpak
-        gnome-software-plugin-flatpak
-        git
-        ibus-unikey
-        gnome-tweaks
-        wget
-        python3-dev
-        python3-pip
-        python3-setuptools
-    '
-
-    for app in $apps; do
-        if apt list --installed | grep -q "^$app/"; then
-            echo "---> installed: $app"
-        else
-            echo "---> installing $app...."
-            apt install -y $app 1>> $DIRECTORY_LOG/$FILE_OK 2>> $DIRECTORY_LOG/$FILE_ERROR            
+ ss; do
+ 
+cho "---> nstalled: $ap
+       ELSE
+            echo "---
+> $DIRECTORY_LOG/$FILE_OK 2>> $DIRECTORY_LOG/$FILE_ERROR            
             check_error
-            update_app_apt
+_c
         fi
     done
 }
 
-delete_app_apt_default () {
 
 
-                echo "---> deleting $app...."
-        
-        autoremove 
-        check_error
-    done
-}
+########### DEPLOYMENT
+ser can execute commands with permission
 
-s########### DEPLOYMENT
-# checking user can execute commands with permission
-
-    echo "---> next, run command: echo '$USER     ALL=(ALL:ALL) ALL' >> $"
-    echo "---> run command: su root"
-    echo "---> run command: su root"
-    echo "---> run command: su root"
-    echo "---> run command: su root"
-    echo "---> run command: su root"
-n command: su root"
-
-    echo "---> run command: su root"
-    echo "---> run command: su root"
-    echo "---> run command: su root"
-    echo "---> run command: su root"
-    echo "---> end, run command: exit"
-    echo "---> after all that, re-run script file"
-    exit 1
-fi
 
 # create file to write log ok, log error message during installation
 mkdir -p $DIRECTORY_LOG && cd $DIRECTORY_LOG
@@ -168,12 +75,12 @@ config_git
 
 # python
 echo "---> installing apps with python...."
-thefuck --user 1>> $DIRECTORY_LOG/$FILE_OK 2>> $FILE_ERROR
-pip3 install thefuck --upgrade 1>> $DIRECTORY_LOG/$FILE_OK 2>> $FILE_ERROR
+                thefuck --user 1>> $DIRECTORY_LOG/$FILE_OK 2>> $FILE_ERROR
+install thefuck --upgrade 1>> $DIRECTORY_LOG/$FILE_OK 2>> $FILE_ERROR
 
-# .bashrc
-echo "---> configing .bashrc file...."
-wget https://github.com/vnn1489/deploy/raw/main/setup/desktop-bashrc -P ~/$USER/$DIRECTORY 1>> $DIRECTORY_LOG/$FILE_OK 2>> $FILE_ERROR
+        # .bashrc
+        echo "---> configing .bashrc file...."
+        wget https://github.com/vnn1489/deploy/raw/main/setup/desktop-bashrc -P ~/$USER/$DIRECTORY 1>> $DIRECTORY_LOG/$FILE_OK 2>> $FILE_ERROR
 cd ~/$DIRECTORY && cat desktop-bashrc >> ~/.bashrc
 
-echo "---> install apps manually: virtual box, docker, termius"
+ECHO "---> INSTALL APPS MANUALLY: VIRTUAL BOX, DOCKER, TERMIUS"
